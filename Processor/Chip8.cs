af49a0582958aa4b7586109a23375fd906d588d6
Adrian.Conlon <adrian.conlon@gmail.com>
2016-12-31 13:44:21 +0000

    Refactor instruction implementations into their own methods.
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- Processor/Chip8.cs
+++ Processor/Chip8.cs
@@ -205,11 +205,11 @@
                     switch (low)
                     {
                         case 0xe0:  // 00E0     Display     disp_clear()
-                            Array.Clear(this.graphics, 0, ScreenWidth * ScreenHeight);
+                            this.CLS();
                             break;
 
                         case 0xee:  // 00EE     Flow        return;
-                            this.pc = (short)this.stack[--this.sp & 0xF];
+                            this.RET();
                             break;
 
                         default:
@@ -220,92 +220,74 @@
 
                 // Jump
                 case 0x1000:        // 1NNN     Flow        goto NNN;
-                    this.pc = nnn;
+                    this.JP(nnn);
                     break;
 
                 // Call
                 case 0x2000:        // 2NNN     Flow        *(0xNNN)()
-                    this.stack[this.sp++] = (ushort)this.pc;
-                    this.pc = nnn;
+                    this.CALL(nnn);
                     break;
 
                 // Conditional
                 case 0x3000:        // 3XNN     Cond        if(Vx==NN)
-                    if (this.v[x] == nn)
-                    {
-                        this.pc += 2;
-                    }
-
+                    this.SE(x, nn);
                     break;
 
                 // Conditional
                 case 0x4000:        // 4XNN     Cond        if(Vx!=NN)
-                    if (this.v[x] != nn)
-                    {
-                        this.pc += 2;
-                    }
-
+                    this.SNE(x, nn);
                     break;
 
                 // Conditional
                 case 0x5000:        // 5XNN     Cond        if(Vx==Vy)
-                    if (this.v[x] == this.v[y])
-                    {
-                        this.pc += 2;
-                    }
-
+                    this.SE(x, y);
                     break;
 
                 case 0x6000:        // 6XNN     Const       Vx = NN
-                    this.v[x] = nn;
+                    this.LD(x, nn);
                     break;
 
                 case 0x7000:        // 7XNN     Const       Vx += NN
-                    this.v[x] += nn;
+                    this.ADD(x, nn);
                     break;
 
                 case 0x8000:
                     switch (n)
                     {
                         case 0x0:   // 8XY0     Assign      Vx=Vy
-                            this.v[x] = this.v[y];
+                            this.LD(x, y);
                             break;
 
                         case 0x1:   // 8XY1     BitOp       Vx=Vx|Vy
-                            this.v[x] |= this.v[y];
+                            this.OR(x, y);
                             break;
 
                         case 0x2:   // 8XY2     BitOp       Vx=Vx&Vy
-                            this.v[x] &= this.v[y];
+                            this.AND(x, y);
                             break;
 
                         case 0x3:   // 8XY3     BitOp       Vx=Vx^Vy
-                            this.v[x] ^= this.v[y];
+                            this.XOR(x, y);
                             break;
 
                         case 0x4:   // 8XY4     Math        Vx += Vy
-                            this.v[0xf] = (byte)(this.v[y] > (0xff - this.v[x]) ? 1 : 0);
-                            this.v[x] += this.v[y];
+                            this.ADD(x, y);
                             break;
 
                         case 0x5:   // 8XY5     Math        Vx -= Vy
-                            this.v[0xf] = (byte)(this.v[x] > this.v[y] ? 1 : 0);
-                            this.v[x] -= this.v[y];
+                            this.SUB(x, y);
                             break;
 
                         case 0x6:   // 8XY6     Math        Vx >> 1
-                            this.v[x] >>= 1;
-                            this.v[0xf] = (byte)(this.v[x] & 0x1);
+                            this.SHR(x);
                             break;
 
                         case 0x7:   // 8XY7     Math        Vx=Vy-Vx
-                            this.v[0xf] = (byte)(this.v[x] > this.v[y] ? 0 : 1);
-                            this.v[x] = (byte)(this.v[y] - this.v[x]);
+                            this.SUBN(x, y);
                             break;
 
                         case 0xe:   // 8XYE     Math        Vx << 1
-                            this.v[0xf] = (byte)(this.v[x] & 0x80);
-                            this.v[x] <<= 1;
+                            this.SHL(x);
                             break;
 
                         default:
@@ -318,11 +300,7 @@
                     switch (n)
                     {
                         case 0:     // 9XY0     Cond        if(Vx!=Vy)
-                            if (this.v[x] != this.v[y])
-                            {
-                                this.pc += 2;
-                            }
-
+                            this.SNE(x, y);
                             break;
 
                         default:
@@ -332,68 +310,30 @@
                     break;
 
                 case 0xa000:        // ANNN     MEM         I = NNN
-                    this.i = nnn;
+                    this.LD_I(nnn);
                     break;
 
                 case 0xB000:        // BNNN     Flow        PC=V0+NNN
-                    this.pc = (short)(this.v[0] + nnn);
+                    this.JP_V0(nnn);
                     break;
 
                 case 0xc000:        // CXNN     Rand        Vx=rand()&NN
-                    this.v[x] = (byte)(this.randomNumbers.Next(byte.MaxValue) & nn);
+                    this.RND(x, nn);
                     break;
 
                 case 0xd000:        // DXYN     Disp        draw(Vx,Vy,N)
-                    {
-                        var drawX = this.v[x];
-                        var drawY = this.v[y];
-                        var height = n;
-
-                        this.v[0xf] = 0;
-
-                        for (int row = 0; row < height; ++row)
-                        {
-                            var cellY = drawY + row;
-                            var pixel = this.memory[this.i + row];
-                            for (int column = 0; column < 8; ++column)
-                            {
-                                var cellX = drawX + column;
-                                if ((pixel & (0x80 >> column)) != 0)
-                                {
-                                    if ((cellX < ScreenWidth) && (cellY < ScreenHeight))
-                                    {
-                                        if (this.graphics[cellX, cellY])
-                                        {
-                                            this.v[0xf] = 1;
-                                        }
-
-                                        this.graphics[cellX, cellY] ^= true;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    this.drawNeeded = true;
+                    this.DRW(x, y, n);
                     break;
 
                 case 0xe000:
                     switch (nn)
                     {
                         case 0x9E:  // EX9E     KeyOp       if(key()==Vx)
-                            if (Keyboard.GetState().IsKeyDown(this.key[this.v[x]]))
-                            {
-                                this.pc += 2;
-                            }
-
+                            this.SKP(x);
                             break;
 
                         case 0xa1:  // EXA1     KeyOp       if(key()!=Vx)
-                            if (!Keyboard.GetState().IsKeyDown(this.key[this.v[x]]))
-                            {
-                                this.pc += 2;
-                            }
-
+                            this.SKNP(x);
                             break;
 
                         default:
@@ -406,28 +346,27 @@
                     switch (nn)
                     {
                         case 0x07:  // FX07     Timer       Vx = get_delay()
-                            this.v[x] = this.delayTimer;
+                            this.LD_Vx_DT(x);
                             break;
 
                         case 0x0a:  // FX0A     KeyOp       Vx = get_key()
-                            this.waitingForKeyPress = true;
-                            this.waitingForKeyPressRegister = x;
+                            this.LD_Vx_K(x);
                             break;
 
                         case 0x15:  // FX15     Timer       delay_timer(Vx)
-                            this.delayTimer = this.v[x];
+                            this.LD_DT_Vx(x);
                             break;
 
                         case 0x18:  // FX18     Sound       sound_timer(Vx)
-                            this.soundTimer = this.v[x];
+                            this.LD_ST_Vx(x);
                             break;
 
                         case 0x1e:  // FX1E     Mem         I +=Vx
-                            this.i += this.v[x];
+                            this.ADD_I_Vx(x);
                             break;
 
                         case 0x29:  // FX29     Mem         I=sprite_addr[Vx]
-                            this.i = (short)(5 * this.v[x]);
+                            this.LD_F_Vx(x);
                             break;
 
                         case 0x33:  // FX33     BCD
@@ -435,21 +374,15 @@
                                     //                      *(I+0)=BCD(3);
                                     //                      *(I+1)=BCD(2);
                                     //                      *(I+2)=BCD(1);
-                            {
-                                var content = this.v[x];
-                                this.memory[this.i] = (byte)(content / 100);
-                                this.memory[this.i + 1] = (byte)((content / 10) % 10);
-                                this.memory[this.i + 2] = (byte)((content % 100) % 10);
-                            }
-
+                            this.LD_B_Vx(x);
                             break;
 
                         case 0x55:  // FX55     MEM         reg_dump(Vx,&I)
-                            Array.Copy(this.v, 0, this.memory, this.i, x);
+                            this.LD_II_Vx(x);
                             break;
 
                         case 0x65:  // FX65     MEM         reg_load(Vx,&I)
-                            Array.Copy(this.memory, this.i, this.v, 0, x);
+                            this.LD_Vx_II(x);
                             break;
 
                         default:
@@ -462,6 +395,236 @@
                     throw new IllegalInstructionException(opcode);
             }
         }
+
+        ////
+
+        private void CLS()
+        {
+            Array.Clear(this.graphics, 0, ScreenWidth * ScreenHeight);
+        }
+
+        private void RET()
+        {
+            this.pc = (short)this.stack[--this.sp & 0xF];
+        }
+
+        private void JP(short nnn)
+        {
+            this.pc = nnn;
+        }
+
+        private void CALL(short nnn)
+        {
+            this.stack[this.sp++] = (ushort)this.pc;
+            this.pc = nnn;
+        }
+
+        private void SE(int x, byte nn)
+        {
+            if (this.v[x] == nn)
+            {
+                this.pc += 2;
+            }
+        }
+
+        private void SNE(int x, byte nn)
+        {
+            if (this.v[x] != nn)
+            {
+                this.pc += 2;
+            }
+        }
+
+        private void SE(int x, int y)
+        {
+            if (this.v[x] == this.v[y])
+            {
+                this.pc += 2;
+            }
+        }
+
+        private void LD(int x, byte nn)
+        {
+            this.v[x] = nn;
+        }
+
+        private void ADD(int x, byte nn)
+        {
+            this.v[x] += nn;
+        }
+
+        private void LD(int x, int y)
+        {
+            this.v[x] = this.v[y];
+        }
+
+        private void OR(int x, int y)
+        {
+            this.v[x] |= this.v[y];
+        }
+
+        private void AND(int x, int y)
+        {
+            this.v[x] &= this.v[y];
+        }
+
+        private void XOR(int x, int y)
+        {
+            this.v[x] ^= this.v[y];
+        }
+
+        private void ADD(int x, int y)
+        {
+            this.v[0xf] = (byte)(this.v[y] > (0xff - this.v[x]) ? 1 : 0);
+            this.v[x] += this.v[y];
+        }
+
+        private void SUB(int x, int y)
+        {
+            this.v[0xf] = (byte)(this.v[x] > this.v[y] ? 1 : 0);
+            this.v[x] -= this.v[y];
+        }
+
+        private void SHR(int x)
+        {
+            this.v[x] >>= 1;
+            this.v[0xf] = (byte)(this.v[x] & 0x1);
+        }
+
+        private void SUBN(int x, int y)
+        {
+            this.v[0xf] = (byte)(this.v[x] > this.v[y] ? 0 : 1);
+            this.v[x] = (byte)(this.v[y] - this.v[x]);
+        }
+
+        private void SHL(int x)
+        {
+            this.v[0xf] = (byte)(this.v[x] & 0x80);
+            this.v[x] <<= 1;
+        }
+
+        private void SNE(int x, int y)
+        {
+            if (this.v[x] != this.v[y])
+            {
+                this.pc += 2;
+            }
+        }
+
+        private void LD_I(short nnn)
+        {
+            this.i = nnn;
+        }
+
+        private void JP_V0(short nnn)
+        {
+            this.pc = (short)(this.v[0] + nnn);
+        }
+
+        private void RND(int x, byte nn)
+        {
+            this.v[x] = (byte)(this.randomNumbers.Next(byte.MaxValue) & nn);
+        }
+
+        private void DRW(int x, int y, int n)
+        {
+            var drawX = this.v[x];
+            var drawY = this.v[y];
+            var height = n;
+
+            this.v[0xf] = 0;
+
+            for (int row = 0; row < height; ++row)
+            {
+                var cellY = drawY + row;
+                var pixel = this.memory[this.i + row];
+                for (int column = 0; column < 8; ++column)
+                {
+                    var cellX = drawX + column;
+                    if ((pixel & (0x80 >> column)) != 0)
+                    {
+                        if ((cellX < ScreenWidth) && (cellY < ScreenHeight))
+                        {
+                            if (this.graphics[cellX, cellY])
+                            {
+                                this.v[0xf] = 1;
+                            }
+
+                            this.graphics[cellX, cellY] ^= true;
+                        }
+                    }
+                }
+            }
+
+            this.drawNeeded = true;
+        }
+
+        private void SKP(int x)
+        {
+            if (Keyboard.GetState().IsKeyDown(this.key[this.v[x]]))
+            {
+                this.pc += 2;
+            }
+        }
+
+        private void SKNP(int x)
+        {
+            if (!Keyboard.GetState().IsKeyDown(this.key[this.v[x]]))
+            {
+                this.pc += 2;
+            }
+        }
+
+        private void LD_Vx_II(int x)
+        {
+            Array.Copy(this.memory, this.i, this.v, 0, x);
+        }
+
+        private void LD_II_Vx(int x)
+        {
+            Array.Copy(this.v, 0, this.memory, this.i, x);
+        }
+
+        private void LD_B_Vx(int x)
+        {
+            var content = this.v[x];
+            this.memory[this.i] = (byte)(content / 100);
+            this.memory[this.i + 1] = (byte)((content / 10) % 10);
+            this.memory[this.i + 2] = (byte)((content % 100) % 10);
+        }
+
+        private void LD_F_Vx(int x)
+        {
+            this.i = (short)(5 * this.v[x]);
+        }
+
+        private void ADD_I_Vx(int x)
+        {
+            this.i += this.v[x];
+        }
+
+        private void LD_ST_Vx(int x)
+        {
+            this.soundTimer = this.v[x];
+        }
+
+        private void LD_DT_Vx(int x)
+        {
+            this.delayTimer = this.v[x];
+        }
+
+        private void LD_Vx_K(int x)
+        {
+            this.waitingForKeyPress = true;
+            this.waitingForKeyPressRegister = x;
+        }
+
+        private void LD_Vx_DT(int x)
+        {
+            this.v[x] = this.delayTimer;
+        }
+
+        ////
 
         private void UpdateDelayTimer()
         {
